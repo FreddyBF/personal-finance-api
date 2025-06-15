829199cb72d5bfe1f974f8f0b5f39ede9fd60af4
Freddy <baptistaalfredo81@gmail.com>
2025-06-15 14:01:54 +0100

    feat: interface para camada de serviço da transaction

---
+++ src/modules/transactions/interfaces/transaction.service.interface.ts
@@ -0,0 +1,26 @@
+import { CreateTransactionDTO } from "../dtos/create-transaction.dto";
+import { TransactionSummaryResponseDTO } from "../dtos/transaction-response-sumary.dto";
+import { TransactionResponseDTO } from "../dtos/transaction-response.dto";
+import { UpdateTransactionDTO } from "../dtos/update-transaction.dto";
+import { TransactionFilters } from "./transaction.filters.interface";
+
+export interface ITransactionService  {
+
+    createTransaction(userId:string, transaction: CreateTransactionDTO): Promise<TransactionResponseDTO>;
+
+    getAll(userId: string): Promise<TransactionResponseDTO[]>;
+
+    getAllByFilters(
+        userId: string, filters?:TransactionFilters
+    ): Promise<TransactionResponseDTO[]>;
+
+    updateOne(
+        userId: string, transactionId: string, transaction: UpdateTransactionDTO
+    ): Promise<TransactionResponseDTO>;
+
+    replaceOne(userId: string, transaction: CreateTransactionDTO): Promise<TransactionResponseDTO>;
+
+    deleteOne(userId: string, transactionId: string): Promise<TransactionResponseDTO>;
+
+    getFinancialSummary(userId: string): Promise<TransactionSummaryResponseDTO>;
+}

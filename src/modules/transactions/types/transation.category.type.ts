37029ad6aa5fed64c12270cb239d9aa01dd3e535
Freddy <baptistaalfredo81@gmail.com>
2025-06-18 05:22:04 +0100

    feat: trasformado em tupla

--- src/modules/transactions/types/transation.category.type.ts
+++ src/modules/transactions/types/transation.category.type.ts
@@ -1,12 +1,14 @@
-export enum TransactionCategory {
-    Alimentacao = "Alimentação",
-    Transporte = "Transporte",
-    Lazer = "Lazer",
-    Saude = "Saúde",
-    Educacao = "Educação",
-    Investimentos = "Investimentos",
-    Moradia = "Moradia",
-    Salario = "Salário",
-    Outros = "Outros",
-}
 
+export const TRANSACTION_CATEGORIES_LIST = [
+    "Alimentação",
+    "Transporte",
+    "Lazer",
+    "Saúde",
+    "Educação",
+    "Investimentos",
+    "Moradia",
+    "Salário",
+    "Outros"
+] as const; // O 'as const' é crucial para inferir um tupla de strings literais.
+
+export type TransactionCategoryType = typeof TRANSACTION_CATEGORIES_LIST[number];
